72695264bce0ef0f224e9172ef9c92eaf7ad2019
Gerald O. Calderon <geraldcalderonofficial@gmail.com>
2025-11-06 14:01:12 +0800

    update files

---
+++ who_will_pay_the_bill.py
@@ -0,0 +1,16 @@
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+# Get the total number of items in list.
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+
+# person_who_will_pay = random.choice(names)
+
+print(person_who_will_pay + " is going to buy the meal today.")
